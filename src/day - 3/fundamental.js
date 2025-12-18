05a3d5b2ab77b43a2d0fee5e464bd679ee34cdb1
jay sevak <2304030100554@silveroakuni.ac.in>
2025-12-18 19:13:09 +0530

    Updated the file fundamental.js with type conversion examples and type checks.

---
+++ src/day - 3/fundamental.js
@@ -0,0 +1,19 @@
+console.log(typeof 10n);
+
+// Type conversion
+let numvalue = "123";
+
+let numConverted = Number(numvalue);
+console.log(typeof numConverted);
+
+console.log(drive());
+
+
+strvalue = Number(numvalue);
+console.log(typeof numvalue);// number
+
+let strnum = 456;
+let strvalue2 = String(strnum);
+console.log(strnum);
+
+// Updated on 2025-12-18
